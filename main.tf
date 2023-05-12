e6cfba0af94a5d1cc0bde384f340e698915eecaa
lcaldeirini <leonardocaldeirini@gmail.com>
2023-05-12 18:31:43 -0300

    fix: test code and change provider function

--- main.tf
+++ main.tf
@@ -6,7 +6,33 @@ terraform {
 }
 
 provider "aws" {
-    profile = "default"
-    shared_credentials_files = "~/.aws/credentials"
-    region = us-east-1
+  region = "us-east-1"
+  profile = "default"
+  shared_credentials_files = ["~/.aws/credentials"]
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform_aws"
+  }
+}
+
