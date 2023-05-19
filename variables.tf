02849251c87902bacd44176a45d8466096a6c2d5
lcaldeirini <leonardocaldeirini@gmail.com>
2023-05-19 15:51:51 -0300

    modificate variables.tf file

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "inst_type" {
+    description = "instance type on ec2 tier"
+    default = "t3.micro"
+}
+
+variable "disable_api_termination" {
+    description = "protect instance by acidental delete"
+    type = bool
+    default = true
+}
+
+variable "int_names" {
+    description = "Lista de nomes para instancias"
+    type        = list(string)
+    default     = ["-", "-", "-", "-"]
+
+}
+
